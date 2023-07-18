f1242a4b4b66fe5b339f813aa58f444ab7490fc3
Raphael Peuckert <raphael@spdigital.agency>
2023-07-18 15:20:44 +0200

    have to finish settings popup

--- project-code/app.js
+++ project-code/app.js
@@ -22,111 +22,97 @@ stageCtn.addEventListener("click", (event) => {
     }
 });
 
+const increaseButtons = document.querySelectorAll('.increase-timer-btn');
+const decreaseButtons = document.querySelectorAll('.decrease-timer-btn');
+for(let increaseBtn of increaseButtons) {
+    increaseBtn.addEventListener('click', preventDefault);
+};
+for(let decreaseBtn of decreaseButtons) {
+    decreaseBtn.addEventListener('click', preventNegative);
+};
+function preventDefault(e) {
+    e.preventDefault();
+}
 
-body.classList.add('ff-1')
+function preventNegative(e) {
+    preventDefault(e);
+    switch(e.target.id) {
+        case 'pomodoro-down':
+            if(Number(document.getElementById('pomodoro').value) <= 0) {
+                document.getElementById('pomodoro').value = 0
+            }
+        case 'short-break-down':
+            if(Number(document.getElementById('short-break').value) <= 0) {
+                document.getElementById('short-break').value = 0
+            }
+        case 'long-break-down':
+            if(Number(document.getElementById('long-break').value) <= 0) {
+                document.getElementById('long-break').value = 0
+            }
 
-
-
-
-
-
+    }
+}
 
 
 function setProgress(progress) {
-  const timerFill = document.querySelector('.timer-fill');
-  const circumference = 2 * Math.PI * 45;
-  const dashOffset = circumference - (progress / 100) * circumference;
-  timerFill.style.strokeDasharray = `${dashOffset} ${circumference}`;
+    const timerFill = document.querySelector('.timer-fill');
+    const circumference = 2 * Math.PI * 45;
+    const dashOffset = circumference - (progress / 100) * circumference;
+    timerFill.style.strokeDasharray = `${dashOffset} ${circumference}`;
 }
 setProgress(0)
 
 
-/* //baseTime is in ms
-const baseTime = 60000
-let currentTime = 60000
-let progressPercantage = currentTime / baseTime * 100; */
+const timerOutEl = document.querySelector('.time');
+const startBtnText = document.querySelector('.start-btn-text').innerText;
 
+let baseVal = 1;
 
-const time = document.querySelector('.time');
+let initialBaseValMs = baseVal * 60000;
+let baseValMs = baseVal * 60000;
+let percentValue = 0;
 
-const setTime = 10000;
-let baseTime = setTime
-time.innerHTML = `${Math.floor(baseTime/60000)}:${Math.floor((baseTime%60000)/1000)}`
-const startBtn = document.querySelector('.start-btn');
-const startBtnTxt = document.querySelector('.start-btn-text');
-console.log(startBtnTxt.innerHTML)
+let paused = false;
 
-let timerStart = Date.now()
-let timeElapsed = 0;
-
-let intervalPaused = false;
-
-startBtn.addEventListener('click', () => {
-    let restart;
-    if(document.querySelector('.paused')){
-        console.log('test1')
-        console.log('test2')
-        startBtnTxt.classList.remove('paused')
-        startBtnTxt.classList.add('running')
-        intervalPaused = false;
-        startBtnTxt.innerHTML = 'pause';
-    } else if(document.querySelector('.running') && !restart){
-        startBtnTxt.classList.remove('running')
-        startBtnTxt.classList.add('paused')
-        intervalPaused = true;
-        startBtnTxt.innerHTML = 'resume';   
+function timer() {
+    if(!paused && baseValMs > 0) {
+        percentValue += 1000;
+        baseValMs -= 1000;
+        let progressPercentage = percentValue*100/initialBaseValMs
+        setProgress(progressPercentage)
+        convertToOutputTime();
+    } else {
+        clearInterval(timer);
     }
-    if(startBtnTxt.innerHTML == 'start') {
-        startBtnTxt.innerHTML = 'pause';
-        startBtnTxt.classList.add('running')
-        
-            const elapsedTime = setInterval(() => {
-                restart = false;
-                if(document.querySelector('.running')) {
-                    const millis = Date.now() - timerStart - 1000
-                    console.log(millis)
-                    if(millis < baseTime + 1000) {
-                        timeElapsed = Math.floor(millis/1000) * 1000;
-            
-                        let outMinutes;
-                        let outSeconds;
-                        if(timeElapsed >= 0) {
-                            const outTime = baseTime - timeElapsed;
-                            outMinutes = Math.floor(outTime/60000);
-                            outSeconds = Math.floor((outTime%60000)/1000);
-                            time.innerHTML = `${outMinutes}:${outSeconds < 10 ? '0' : ''}${outSeconds}`;
-                        }
-                        
-                
-                        time.innerHTML = `${outMinutes}:${outSeconds < 10 ? '0' : ''}${outSeconds}`;
-                
-                        const progressPercantage = timeElapsed/baseTime*100;
-                        setProgress(progressPercantage);
-                    } else {
-                        timeElapsed = 0;
-                        startBtnTxt.innerHTML = 'restart';
-                        clearInterval(elapsedTime);
-                        console.log('Timer stopped!')
-                    }
-                    
-                     
-                }
-                
-            }, 1000)
-        
-    } else if(startBtnTxt.innerHTML == 'restart') {
-        startBtnTxt.classList.add('running')
-        startBtnTxt.innerHTML = 'start';
-        baseTime = setTime
-        timerStart = Date.now();
-        const outTime = baseTime - timeElapsed;
-        outMinutes = Math.floor(outTime/60000);
-        outSeconds = Math.floor((outTime%60000)/1000);
-        time.innerHTML = `${outMinutes}:${outSeconds < 10 ? '0' : ''}${outSeconds}`;
-        const progressPercantage = timeElapsed/baseTime*100;
-        setProgress(progressPercantage);
-        restart = true;
-    } 
-})
+}
+
+function startInterval(){
+    setInterval(timer, 1000);
+} 
+
+function pauseTimer(){
+    paused = true;
+    startBtnText = 'resume'
+}
+
+function resumeTimer(){
+    paused = false;
+}
+
+function restartTimer(){
+    paused = false;
+}
+
+convertToOutputTime();
+
+function convertToOutputTime(){
+    let outSeconds = Math.floor(baseValMs%60000)/1000;
+    let outMinutes = Math.floor(baseValMs/60000)%1000;
+    timerOutEl.innerText = outMinutes.toString().padStart(2, '0') + ":" + outSeconds.toString().padStart(2, '0');
+}
+
+
+body.classList.add('ff-1');
+
 
 
